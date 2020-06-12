73a1b328e8ed718b61843a2dd84809632be56dc2
Gui-dev <adrianon10@gmail.com>
2020-06-12 03:19:20 -0300

    Implementando o PointController e o metodo store, configurando a rota post de points

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,8 +3,11 @@ import { Router, Request, Response } from 'express'
 const routes = Router()
 
 import Items from './../controllers/ItemsController'
+import Point from './../controllers/PointController'
 
 routes.get( '/items', Items.index )
+
+routes.post( '/points', Point.store )
 
 
 export default routes
