796d92212495cacf3abb24fedd3f3a11bfc2d63d
Gui-dev <adrianon10@gmail.com>
2020-06-12 03:20:42 -0300

    Fix: metodo index para retornar o id do item

--- backend/src/controllers/ItemsController.ts
+++ backend/src/controllers/ItemsController.ts
@@ -8,6 +8,7 @@ class ItemsController {
     const items = await knex( 'items' ).select( '*' )
     const serializedItems = items.map( item => {
       return {
+        id: item.id,
         title: item.title,
         image_url: `http://192.168.0.105:3333/uploads/${item.image}`
       }
