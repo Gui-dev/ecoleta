eef0d576338e6139d7729b1464625bd55ed1e606
Gui-dev <adrianon10@gmail.com>
2020-06-16 02:10:24 -0300

    Configurando a página CreatePoint

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import Home from './../pages/Home'
+import CreatePoint from './../pages/CreatePoint'
 
 const Routes: React.FC = () => {
 
   return (
     <Switch>
       <Route path="/" exact component={ Home }/>
+      <Route path="/create-point" exact component={ CreatePoint }/>
     </Switch>
   )
 }
